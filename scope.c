eb32541debaa737ebc57d8e2c3064222ffcea6ac
CurtisNewbie <zhuangyongj@gmail.com>
2020-03-30 10:57:05 +0100

    Add example of static var in function

--- scope.c
+++ scope.c
@@ -2,15 +2,18 @@
 
 void withExtern(void);
 void withoutExtern(void);
+void printStaticArr(void);
 
 // this cannot be accessed by other c files unless using extern
 // this protects the other files from using this variable unintentionally
-static int staticNumber = 10;
+static int staticNumber
+    = 10;
 
 int main()
 {
     withExtern();
     withoutExtern();
+    printStaticArr();
 }
 
 void withExtern(void)
@@ -26,4 +29,12 @@ void withoutExtern(void)
     // appears to be some random number
     int staticNumber;
     printf("Without extern: %d\n", staticNumber);
+}
+
+// https://stackoverflow.com/questions/572547/what-does-static-mean-in-c
+void printStaticArr(void)
+{
+    // A static variable inside a function keeps its value between invocations.
+    static char arr[] = "I am a static array in function";
+    printf("%s\n", arr);
 }
